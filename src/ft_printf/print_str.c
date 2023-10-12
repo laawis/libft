bce7d06b0193f22adaad5fae9131e44ab3ccaeb5
laawis <ollier.gary@gmail.com>
2023-10-13 00:52:40 +0200

    ft_printf->print_str->update

--- src/ft_printf/print_str.c
+++ src/ft_printf/print_str.c
@@ -1,22 +1,10 @@
 #include <unistd.h>
-
+#include <libft.h>
 
 #include "ft_printf.h"
-#include "libft.h"
-
-size_t    ft_strlen(const char *str)
-{
-    size_t    i;
-
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
 
 int print_str(char *str)
 {
-
     if (str == NULL)
     {
         return (-1);
